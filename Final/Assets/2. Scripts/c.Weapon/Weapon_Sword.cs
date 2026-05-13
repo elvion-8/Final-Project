9bd53080327ecccc601037104eb25cb2a5d8a840
DESKTOP-1SE37VB\user <wjswns05@naver.com>
2026-05-13 16:20:07 +0900

    웨펀베이스 기준으로 기타웨펀들 수정

--- Final/Assets/2. Scripts/c.Weapon/Weapon_Sword.cs
+++ Final/Assets/2. Scripts/c.Weapon/Weapon_Sword.cs
@@ -1,102 +1,12 @@
-using JetBrains.Annotations;
 using UnityEngine;
-using UnityEngine.UI;
 
-public class Weapon_Sword : MonoBehaviour, IWeaponStats
+
+public class Weapon_Sword : scWeaponBase
 {
-    public int attackDmg { get; private set;} = 50;       //공격력
-    public float attackRange { get; private set;}=5f;    //공격 범위
-    public float attackCoolDown { get; private set;}=1f;  //공격 쿨다운
-    public int attackType { get; private set;}=1;         //공격 타입
-    public float weaponMoveSpeed { get; private set;}=1f; //무기 이동속도
-    public float attackSpeed { get; private set;}=2f;   //공격 속도
-    public int criticalDmg {get; private set;}=2;      //치명타 데미지
-    public int criticalProb {get; private set;}=10;     //치명타 확률
-    //public WeaponType Type { get; private set;}=      //무기 종류
-
-    public int Durability { get; private set; }  = 150;  //내구도 
-
-    int damage;
-
-    float HitTime;
     
-    float atSpeed()
-    {
-        return attackSpeed;
-    }
+    public override void Skill1()
+    {}
+    public override void Skill2()
+    {}
     
-
-    void Awake()
-    {
-        //애니메이션 컴포넌트 연결
-
-        
-    }
-    //[ContextMenu.test("")]
-    public void Equip()                //무기 착용
-    {Debug.Log("스워드임");
-    Debug.Log(attackDmg);}
-
-    public void Skill1()
-    {}
-    public void Skill2()
-    {}
-    void OnTriggerEnter(Collider other)
-    {
-        //ITakeDamage target = other.GetComponent<ITakeDamage>();
-
-        //if(target != null)
-        //{
-        //    target.TakeDamage(attackDmg);
-        //}
-
-        if (GameObject.FindWithTag("Player").GetComponent<PlayerCtrl>().isAttacking == true)
-        {
-            if (other.gameObject.tag == "Enemy")
-            {
-                if (Time.time < HitTime + attackSpeed)
-                {
-                    // 아직 무적 시간 중이라면 데미지 무시
-                    return;
-                }
-
-                other.GetComponent<EnemyCtrl>().TakeDamage(Damage());
-
-                //내구도 감소
-                Durability -= 10;
-
-                //피격된 시간
-                HitTime = Time.time;
-
-                if (Durability <= 0)
-                {
-                    WeaponDie();
-                }
-
-            }
-        }
-        
-    }
-
-
-    public int Damage()
-    {
-        int multiplier = 1;
-
-        if (Random.Range(0, 30) < criticalProb)
-        {
-            multiplier = criticalDmg;
-            Debug.Log("크리티컬 히트!");
-        }
-
-        damage = multiplier * attackDmg;
-        
-        return damage;
-    }
-
-    void WeaponDie()
-    {
-        Debug.Log("내구도 0");
-        Destroy(gameObject);
-    }
 }
--- Final/Assets/2. Scripts/c.Weapon/scWeaponBase.cs
+++ Final/Assets/2. Scripts/c.Weapon/scWeaponBase.cs
@@ -24,8 +24,11 @@ public abstract class scWeaponBase : MonoBehaviour, IWeaponStats
             return baseCritDmg+(pS.weaponCritDmgUpgradeCnt);}}      //치명타 데미지
     public int criticalProb { get{if (pS == null) return baseCritProb;
             return baseCritProb+(pS.weaponCritProbUpgradeCnt);}}     //치명타 확률
-    public int Durability { get; }     //내구도
+    public int Durability { get; private set; }     //내구도
 
+    int damage;                        //데미지
+
+    float HitTime;                     //히트후 무적시간
 
     public void Equip()                //무기 착용
     {}
@@ -33,4 +36,61 @@ public abstract class scWeaponBase : MonoBehaviour, IWeaponStats
     public abstract void Skill1();               //무기별 기본 스킬 2개 정도는 넣기!
 
     public abstract void Skill2();
+
+    void OnTriggerEnter(Collider other)
+    {
+        //ITakeDamage target = other.GetComponent<ITakeDamage>();
+
+        //if(target != null)
+        //{
+        //    target.TakeDamage(attackDmg);
+        //}
+
+        if (GameObject.FindWithTag("Player").GetComponent<PlayerCtrl>().isAttacking == true)
+        {
+            if (other.gameObject.tag == "Enemy")
+            {
+                if (Time.time < HitTime + attackSpeed)
+                {
+                    // 아직 무적 시간 중이라면 데미지 무시
+                    return;
+                }
+                other.GetComponent<EnemyCtrl>().TakeDamage(Damage());
+
+                //내구도 감소
+                Durability -= 10;
+
+                //피격된 시간
+                HitTime = Time.time;
+
+                if (Durability <= 0)
+                {
+                    WeaponDie();
+                }
+            }
+        }
+
+    }
+
+
+    public int Damage()
+    {
+        int multiplier = 1;
+
+        if (Random.Range(0, 30) < criticalProb)
+        {
+            multiplier = criticalDmg;
+            Debug.Log("크리티컬 히트!");
+        }
+
+        damage = multiplier * attackDmg;
+
+        return damage;
+    }
+
+    void WeaponDie()
+    {
+        Debug.Log("내구도 0");
+        Destroy(gameObject);
+    }
 }
