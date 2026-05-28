07dbfb7aa4b39ee371058ecab2e3c8130ee125ae
hexionDh <engus99@gmail.com>
2026-05-28 17:50:25 +0900

    약간의 수정(All Test에 InputSystem 추가)

--- Final/Assets/2. Scripts/a.Character/a-2.Player/PlayerCtrlNew.cs
+++ Final/Assets/2. Scripts/a.Character/a-2.Player/PlayerCtrlNew.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Unity.VisualScripting;
 
 public class PlayerCtrlNew : MonoBehaviour, ITakeDamage
 {
