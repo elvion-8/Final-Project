49a7346da15e0ea0857a32821ad2f5dc2239c6fd
DESKTOP-1SE37VB\user <wjswns05@naver.com>
2026-06-05 16:38:31 +0900

    도끼스킬 추가

---
+++ Final/Assets/2. Scripts/c.Weapon/Skill.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Skill : MonoBehaviour
+{
+    //Axe ÆÄÆ¼Å¬
+    public ParticleSystem rockParticle;
+
+    public Transform playerPoint;
+
+    public void PlayRockParticle()
+    {
+        playerPoint = GameObject.Find("Player").transform;
+
+        if(rockParticle != null && playerPoint != null)
+        {
+            Vector3 usPoint = playerPoint.position + (playerPoint.forward * 2.5f);
+
+            ParticleSystem instance = Instantiate(rockParticle, usPoint, playerPoint.rotation);
+            instance.Play();
+
+            Destroy(instance.gameObject, 1);
+
+        }
+    }
+}
