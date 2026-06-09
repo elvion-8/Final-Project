4962789a2e6dd539116cd4c68b4d019bb539b3ef
DESKTOP-1SE37VB\user <wjswns05@naver.com>
2026-06-09 15:16:10 +0900

    창 스킬사용시 이동추가

--- Final/Assets/2. Scripts/c.Weapon/Weapon_Spear.cs
+++ Final/Assets/2. Scripts/c.Weapon/Weapon_Spear.cs
@@ -4,11 +4,27 @@ using UnityEngine;
 
 public class Weapon_Spear: scWeaponBase
 {
+    PlayerCtrl player;
+    private int tempJumpCnt;
+    Transform pTr;
+
     public Animator anim;
 
     public Vector3 z35 = new Vector3(0, 0, 35);
 
-    WaitForSeconds delay = new WaitForSeconds(2.5f);
+    WaitForSeconds delay1 = new WaitForSeconds(0.4f);
+
+    WaitForSeconds delay2 = new WaitForSeconds(1.0f);
+
+    WaitForSeconds delay3 = new WaitForSeconds(0.04f);
+
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerCtrl>();
+        pTr = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
+        anim = GameObject.Find("Player").GetComponentInChildren<Animator>();
+    }
 
     public override void Skill1()
     {
@@ -22,11 +38,25 @@ public class Weapon_Spear: scWeaponBase
         transform.localRotation = Quaternion.Euler(-z35);
 
         Debug.Log("스킬애니1");
-        anim = GameObject.Find("Player").GetComponentInChildren<Animator>();
-
+       
         anim.SetTrigger("Skill1");
 
-        yield return delay;
+        RaycastHit rayMove;
+
+        yield return delay1;
+
+        for (int i = 0; i < 16; i++)
+        {
+            bool SkillTrue = Physics.Raycast(pTr.position + Vector3.up * 1.0f, pTr.forward / 2, out rayMove, 10);
+            if (!SkillTrue)
+            {
+                pTr.position += pTr.forward / 2;
+            }
+
+            yield return delay3;
+        }
+
+        yield return delay2;
 
         transform.localRotation = Quaternion.Euler(Vector3.zero);
 
